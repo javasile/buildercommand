1b596e83314a4c6b912f1aa667d320edf38cbf42
golde <goldeanu_klau_clau@yahoo.co.uk>
2021-11-29 14:41:24 +0200

    start builder + command

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,12 @@
+import java.time.LocalDate;
+
 public class Product {
+    private String name;
+    double price;
+    int quantity;
+    String color;
+    float weight;
+    String descreiption;
+    LocalDate date;
+
 }
